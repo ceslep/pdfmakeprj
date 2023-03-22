6cb746ce40b133a099720f967f96dc7815efc4b2
ceslep <ceslep@gmail.com>
2023-03-22 15:07:27 -0500

    conImagenes

--- src/lib/components/PrinterPdfmake.svelte
+++ src/lib/components/PrinterPdfmake.svelte
@@ -4,8 +4,10 @@
   (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
   import * as htmlToImage from "html-to-image";
   import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
-
+  import { _docDefinition } from "./DocDefincitionsTablesExample.svelte";
+  import { _docDefinitionImages } from "./DocDefinitionsImages.svelte";
   const docDefinition = {
+    compress: false,
     content: [
       {
         text: "Tables",
@@ -55,15 +57,16 @@
   };
 
   let DIV;
-  
+
   const imprimirImagen = async () => {
     // Convertir el contenido HTML a una imagen utilizando html-to-image
     try {
       let dataUrl = await htmlToImage.toPng(DIV, { quality: 1 });
       // Crear el objeto de imagen pdfmake utilizando la imagen convertida
-      let image = { image: dataUrl, fit: ['500', '500'] };
+      let image = { image: dataUrl, fit: ["500", "500"] };
       // Crear el documento pdfmake con la imagen
       let docDefinition = {
+        compress: false,
         content: [image],
       };
       // Generar el archivo PDF y descargarlo en el navegador
@@ -73,33 +76,62 @@
     }
   };
 
-  const qrGenerator=()=>{
-    let docDefinition={
-      content:[
-        { qr: 'Cesar Leandro Patino Velez', foreground: 'black', background: 'yellow' }
-      ]
-    }
+  const qrGenerator = () => {
+    let docDefinition = {
+      compress: false,
+      content: [
+        {
+          qr: "Cesar Leandro Patino Velez",
+          foreground: "black",
+          background: "yellow",
+        },
+      ],
+    };
     pdfMake.createPdf(docDefinition).open();
-  }
+  };
+
+  const Tablas = () => {
+    pdfMake.createPdf(_docDefinition).open();
+  };
+
+  const Imagenes = () => {
+    pdfMake.createPdf(_docDefinitionImages).open();
+  };
 </script>
 
-<button class="btn btn-primary" on:keydown={null} on:click={printer}
+<button class="btn btn-primary rounded-0" on:keydown={null} on:click={printer}
   >Imprimir</button
 >
-<button class="btn btn-success" on:keydown={null} on:click={imprimirImagen}>
+<button
+  class="btn btn-success rounded-0"
+  on:keydown={null}
+  on:click={imprimirImagen}
+>
   Imagen
 </button>
 
-<button class="btn btn-warning" on:keydown={null} on:click={qrGenerator}>Qr</button>
+<button
+  class="btn btn-warning rounded-0"
+  on:keydown={null}
+  on:click={qrGenerator}>Código Qr</button
+>
+
+<button class="btn btn-warning rounded-0" on:keydown={null} on:click={Tablas}
+  >Tablas</button
+>
+
+<button class="btn btn-warning rounded-0" on:keydown={null} on:click={Imagenes}
+  >Imagenes</button
+>
 
 <div bind:this={DIV}>
   <div class="text-center">
-  <img
-    src="https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg"
-    alt=""
-    class="img-fluid"
-  />
-</div>
+    <img
+      src="https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg"
+      alt=""
+      class="img-fluid"
+    />
+  </div>
   <ul class="fs-3">
     {#each [0, 1, 2, 3, 4] as item}
       <li class="text-danger">
